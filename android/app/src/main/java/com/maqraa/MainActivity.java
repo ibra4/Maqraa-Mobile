5dc028fafc6beeafcf9a86190722752452c51bd5
ibra4 <ibrahim@sprintive.com>
2021-12-17 22:19:30 +0200

    Set Splash screen and logo for android

--- android/app/src/main/java/com/maqraa/MainActivity.java
+++ android/app/src/main/java/com/maqraa/MainActivity.java
@@ -1,5 +1,8 @@
 package com.maqraa;
 
+import android.os.Bundle; // here
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Maqraa";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 }
